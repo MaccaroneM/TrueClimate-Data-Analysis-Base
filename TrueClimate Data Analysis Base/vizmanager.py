e5c2c2598381af8c42968b4c0364b3a41c7114c1
MaccaroneM <maccarone.max@pingree.org>
2026-05-26 13:28:41 -0400

    Delete TrueClimate Data Analysis Base/vizmanager.py

--- TrueClimate Data Analysis Base/vizmanager.py
+++
@@ -1,7 +0,0 @@
-#Will be the main realm where most of the dat viz will be calculated and stored for use on website,
-#the MAIN program is the architect.
-
-class VizManager:
-    #def __init__(self):
-
-    #various functions will populate from here as I work.
